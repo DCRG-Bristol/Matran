f78cf6e92582ed637531c60beb66961a170b2b57
sa14378bris <sa14378@bristol.ac.uk>
2025-06-03 18:27:42 +0100

    improved RBE3 element
    still not quite right

---
+++ tbx/+mni/+printing/+cards/RBE3.m
@@ -0,0 +1,89 @@
+classdef RBE3 < mni.printing.cards.BaseCard
+    %FLUTTER_CARD Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        EID;
+        REFGRID;
+        REFC;
+        WTi;
+        Ci;
+        Gij;
+        UM;
+        GMi;
+        CMi;
+        Alpha;
+    end
+    
+    methods
+        function obj = RBE3(EID,REFGRID,REFC,WTi,Ci,Gij,varargin)
+            %GRID_CARD Construct an instance of this class
+            %   Detailed explanation goes here
+            p = inputParser();
+            p.addRequired('EID',@(x)x>0)
+            p.addRequired('REFGRID',@(x)x>0)
+            p.addRequired('REFC',@(x)x>0)
+            p.addRequired('WTi')
+            p.addRequired('Ci',@(x)x>0)
+            p.addRequired('Gij',@(x)x>0)
+            p.addParameter('UM')
+            p.addParameter('GMi',@(x)~any(x<=0))
+            p.addParameter('CMi',@(x)~any(x<=0))
+            p.addParameter('Alpha','')
+            p.parse(EID,REFGRID,REFC,WTi,Ci,Gij,varargin{:})
+            
+            obj.Name = 'MPC';
+            obj.EID     = p.Results.EID;
+            obj.REFGRID = p.Results.REFGRID;
+            obj.REFC    = p.Results.REFC;
+            obj.WTi     = p.Results.WTi;
+            obj.Ci      = p.Results.Ci;
+            obj.Gij     = p.Results.Gij;
+            obj.UM      = p.Results.UM;
+            obj.GMi     = p.Results.GMi;
+            obj.CMi     = p.Results.CMi;
+            obj.Alpha   = p.Results.Alpha;
+
+            % obj.EID = p.Results.EID;
+            % obj.GN = p.Results.GN;
+            % obj.CM = p.Results.CM;
+            % obj.GMi = p.Results.GMi;
+            % obj.Alpha = p.Results.Alpha;           
+        end
+        
+        function writeToFile(obj,fid,varargin)
+            %writeToFile print DMI entry to file
+            writeToFile@mni.printing.cards.BaseCard(obj,fid,varargin{:})
+            data = {obj.EID};
+            format = 'i';
+            j=1;
+            data = [data,{obj.REFGRID},{obj.REFC},{obj.WTi(1)}];
+
+            for i = 2:length(obj.WTi)
+                data = [data,{obj.Gi(i)},{obj.Ci(j)},{obj.WTi(i)},];
+                format = [format,'iir'];
+            end
+
+           
+            % for i = 1:length(obj.Gij)
+            %     data = [data,{obj.Gij(i)}];
+            %     format = [format,'i'];
+            % end
+            % data = [data,{obj.UM}];
+            % format = [format,'s'];
+            % for i = 1:length(obj.GMi)
+            %     data = [data,{obj.GMi(i)}];
+            %     format = [format,'i'];
+            % end
+            % for i = 1:length(obj.CMi)
+            %     data = [data,{obj.CMi(i)}];
+            %     format = [format,'i'];
+            % end
+            % data = [data,{obj.Alpha}];
+            % format = [format,'r'];
+            % 
+            obj.fprint_nas(fid,format,data);
+        end
+    end
+end
+
